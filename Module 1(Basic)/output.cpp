4823bb37c762a02823806130dc126117ab0bec70
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-10-11 04:27:59 +0600

    basic-c

---
+++ Module 1(Basic)/output.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 1st
+    // std::cout << "Hello World";
+
+    // new line
+    // std::cout << "Hello\nworld";
+    // std::cout << "Hello" << std::endl << "World";
+
+    // printing variable
+    int a = 1000, b = 2000;
+    // std:: cout << a;
+    // std::cout << a << b;
+    // std::cout << a << " " << b;
+    cout << a << " " << b << endl;
+
+    return 0;
+}
