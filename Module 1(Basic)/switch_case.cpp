8e97b895800e87ac4d9266d29e271ea8bb7c38f2
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-10-11 15:58:41 +0600

    switch-case-2

--- Module 1(Basic)/switch_case.cpp
+++ Module 1(Basic)/switch_case.cpp
@@ -5,7 +5,26 @@ int main(){
 
     int value;
     cin >> value;
+    switch(value){
+        case 1:
+            cout << "One";
+            break;
+        case 2:
+            cout << "Two";
+            break;
+        case 3:
+            cout << "Three";
+            break;
+        case 4:
+            cout << "Four";
+            break;
+        case 5:
+            cout << "Five";
+            break;
+        default:
+            cout << "Didn't match";
+    }
 
-    cout << value;
+    // cout << value;
     return 0;
 }
