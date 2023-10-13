153951c1a941c3b0a1480d66ac97402cd5ad754d
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-10-13 22:48:15 +0600

    runtime-error

--- Module 2(Dyanmic_memory)/static.cpp
+++ Module 2(Dyanmic_memory)/static.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 int main()
 {
-    
+    // stack => compile
+    // heap => runtime
+
+    // runtime error 
+    int a = 10/0;
+
+    printf("%d", a);
       
     return 0;
 }
