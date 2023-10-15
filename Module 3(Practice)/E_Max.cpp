12576b034be69fc0027d238f0ab1f6e624033d8f
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-10-16 00:43:45 +0600

    e-max-solved

--- Module 3(Practice)/E_Max.cpp
+++ Module 3(Practice)/E_Max.cpp
@@ -4,7 +4,21 @@ using namespace std;
 int main()
 {
     int n;
-    
-      
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int ma = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        ma = max(ma, arr[i]);
+    }
+
+    cout << ma;
+
     return 0;
 }
