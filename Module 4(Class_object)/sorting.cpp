4d8d6f6cc2378862323f9be0b8abc41681bd6ed0
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-10-17 03:29:14 +0600

    sorting

--- Module 4(Class_object)/sorting.cpp
+++ Module 4(Class_object)/sorting.cpp
@@ -3,23 +3,24 @@ using namespace std;
 
 int main()
 {
-    int n ; cin >> n;
+    int n;
+    cin >> n;
     int arr[n];
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
-    // sorting
-    sort(arr, arr+n, greater<int>{});
+    // sorting ascending
+    sort(arr, arr + n);
 
+    // sorting descending
+    sort(arr, arr + n, greater<int>{});
 
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
-    
-    
-      
+
     return 0;
 }
