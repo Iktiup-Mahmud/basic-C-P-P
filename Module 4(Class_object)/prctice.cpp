c80ab0b71f6c96c4210058421f7b2bfbc027f3f6
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-10-17 23:39:27 +0600

    practice

---
+++ Module 4(Class_object)/prctice.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    class Student{
+        public:    
+            int cls;
+            int roll;
+            char sec;
+            string name;
+
+        Student (int c, int r, char s, string n){
+            cls = c;
+            roll
+        }
+    }
+      
+    return 0;
+}
