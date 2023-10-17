c93ceb39f0672e8e8de58e7ec785d3825b18a024
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-10-17 23:43:30 +0600

    static-data

--- Module 4(Class_object)/prctice.cpp
+++ Module 4(Class_object)/prctice.cpp
@@ -1,20 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Student
+{
+public:
+    int cls;
+    int roll;
+    char sec;
+    string name;
+
+    Student(int c, int r, char s, string n)
+    {
+        cls = c;
+        roll = r;
+        sec = s;
+        name = n;
+    }
+};
+
+Student func()
+{
+    Student rahim = Student(9, 16, 'A', "Rahim Ullah");
+    return rahim;
+}
+
 int main()
 {
-    class Student{
-        public:    
-            int cls;
-            int roll;
-            char sec;
-            string name;
+    Student a = func();
 
-        Student (int c, int r, char s, string n){
-            cls = c;
-            roll
-        }
-    }
-      
+    cout << a.cls << endl;
+    cout << a.sec << endl;
+
     return 0;
 }
