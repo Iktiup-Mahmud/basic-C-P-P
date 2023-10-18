60e2420cc7c9032a82ecc7f21e93e275082cf10f
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-10-19 03:53:56 +0600

    revision

---
+++ Module 5(practice)/pointer_test.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char a = 'S';
+    char *p = &a;
+
+    // int a = 'A';
+    // int *p = &a;
+
+    cout << &a << endl;
+    cout << p << endl;
+    cout << *p << endl;
+
+    return 0;
+}
