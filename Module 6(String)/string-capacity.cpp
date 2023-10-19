7c09e1805f961854ed99ae8954c0336eb808b67f
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-10-20 02:43:52 +0600

    string

---
+++ Module 6(String)/string-capacity.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s= "Hello world!";
+
+    // cout << s.size() <<endl;
+    // cout << s.length() <<endl;
+    // cout << s.max_size() <<endl;
+      
+    return 0;
+}
