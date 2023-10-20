a96cfa7e6e14bcbe995d247a35e5044f11cba44b
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-10-21 02:53:56 +0600

    a-prblm-solved

---
+++ Module 6(String)/farmer.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    while (n != 0)
+    {
+        int m1, m2, d;
+        scanf("%d%d %d", &m1, &m2, &d);
+        // cout << m1 << " " << m2 << " " << d << " " << n << endl;
+
+        int after_day = (m1 * d) / (m1 + m2);
+
+        // cout << d - after_day << endl;
+        // cout << d << endl;
+        printf("%d \n", d - after_day);
+        n--;
+    }
+
+    return 0;
+}
---
+++ Module 6(String)/iteration.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    // for (int i = 0; i < s.length(); i++)
+    // {
+    //     cout << s[i]<< endl;
+    // }
+
+    cout << *s.begin() << endl;
+    cout << *(s.end() -1) << endl;
+    
+      
+    return 0;
+}
