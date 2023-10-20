2c2d19ee1e236d4e160b2c21533d17290ade14b2
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-10-20 22:33:50 +0600

    string-modify

--- Module 6(String)/modifiers.cpp
+++ Module 6(String)/modifiers.cpp
@@ -10,11 +10,21 @@ int main()
     // a.append(b);
 
     // a += 'a';
-    a.push_back('x');
-    a.pop_back();
+    // a.push_back('x');
+    // a.pop_back();
 
-    cout << a << endl;
-    cout << b << endl;
+    // a = "Gelo"; /*mostly used*/
+    // a.assign("gelo");
+
+    string c = "Baiust";
+    // c.erase(3, 1);
+    // c.replace(4, 2, "ij");
+    c.replace(4, 0, "ij");
+    // c.insert(4, "ABCD");
+
+    // cout << a << endl;
+    // cout << b << endl;
+    cout << c << endl;
 
     return 0;
 }
