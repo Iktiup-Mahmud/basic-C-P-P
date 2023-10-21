463d4f1175dd8b135d9ee6f6f0adf7b66a49b3ac
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-10-21 21:04:28 +0600

    sorting

---
+++ Module 7(More String and Class)/string_constructor.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string s= "Hello World";
+
+    // 1
+    // string s("Hello world");
+
+    // 2
+    // string s("Hello world", 4);
+
+    // 3
+    // string a = "Hello World";
+    // string s(a, 4);
+
+    // 4
+    string s(5, 'A');
+
+    cout << s << endl;
+
+    return 0;
+}
