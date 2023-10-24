1807c9560b4cc2bca22bd09bf12b5a9d7dfeb6ad
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-10-25 00:59:30 +0600

    reverse-solved-2

--- Module 8.5(Practice)/reverse_class_array.cpp
+++ Module 8.5(Practice)/reverse_class_array.cpp
@@ -8,6 +8,19 @@ public:
     int roll, marks;
 };
 
+void func(Student *a, int n)
+{
+    for (int i = 0; i < n / 2; i++)
+    {
+        for (int j = n - 1; j < n / 2; j--)
+        {
+            // swap(a[i], [j]);
+            Student temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+}
 
 int main()
 {
@@ -20,7 +33,9 @@ int main()
     {
         cin >> arr[i].name >> arr[i].roll >> arr[i].marks;
     }
-    for (int i = n-1; i >= 0; i--)
+
+    func(arr, n);
+    for (int i = n - 1; i >= 0; i--)
     {
         cout << arr[i].name << " " << arr[i].roll << " " << arr[i].marks << endl;
     }
