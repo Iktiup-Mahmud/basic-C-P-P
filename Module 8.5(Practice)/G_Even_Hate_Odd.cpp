036e776ad1ea3959e055e64e171867e0200bbd23
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-10-24 23:38:19 +0600

    g-solved

--- Module 8.5(Practice)/G_Even_Hate_Odd.cpp
+++ Module 8.5(Practice)/G_Even_Hate_Odd.cpp
@@ -4,23 +4,77 @@ using namespace std;
 int main()
 {
     int n;
-    cin>> n;
+    cin >> n;
 
     while (n--)
     {
-        int a;cin>> a;
+        int a;
+        cin >> a;
         int arr[a];
         for (int i = 0; i < a; i++)
         {
-            cin>> arr[i];
+            cin >> arr[i];
         }
-        for (int i = 0; i < a; i++)
+
+        int res = 0;
+        if (a % 2 != 0)
         {
-            cout << arr[i]<< " ";
+            res = -1;
         }
-        cout << endl;
+        else
+        {
+            int even = 0, odd = 0;
+            // if()
+            for (int i = 0; i < a; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    even++;
+                }
+                else
+                {
+                    odd++;
+                }
+            }
+
+            if (even - odd == 0)
+            {
+                res = 0;
+            }
+            else
+            {
+                if (even > odd)
+                {
+                    int cnt = 0;
+                    while (even != odd)
+                    {
+                        cnt++;
+                        even--;
+                        odd++;
+                    }
+                    res = cnt;
+
+                    // cout << "1st";
+                }
+                else
+                {
+                    int cnt = 0;
+                    while (even != odd)
+                    {
+                        cnt++;
+                        even++;
+                        odd--;
+                    }
+                    res = cnt;
+                }
+                // cout << "2nd" << a << res;
+            }
+
+            // cout << even << " "<< odd<< endl;
+        }
+            // cout << res << " " << a << endl;
+            cout << res << endl;
     }
-    
-      
+
     return 0;
 }
