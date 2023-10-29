5fc0022a5edb35cfe60de5c73ae74a7ffd3669c7
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-10-29 20:46:27 +0600

    function_solved-hacker-rank

---
+++ Module 9(Final)/Functions.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int func(int a, int b, int c, int d){
+    if(a>b && a>c && a>d){
+        return a;
+    }
+    else if (b>a && b > c && b > d)
+    {
+        return b;
+    }
+    else if (c > b && c > a && c > d)
+    {
+        return c;
+    }else{
+        return d;
+    }
+};
+
+
+int main()
+{
+    int a, b, c, d;
+    cin >> a>> b>> c>> d;
+    int x = func(a, b, c ,d);
+    cout << x;
+
+    return 0;
+}
