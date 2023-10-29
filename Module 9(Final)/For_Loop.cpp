b6b08a72fde4d77ed277367166a34a3b44ba9e2c
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-10-29 20:28:16 +0600

    for-loop

---
+++ Module 9(Final)/For_Loop.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    // cout << n << m;
+    for (int i = n; i < m; i++)
+    {
+        // int x = m - n;
+
+        // if (x <= 9 && x >= 1)
+        // {
+        //     if (x == 1)
+        //     {
+        //         cout << "one" << endl;
+        //     }
+        //     else if (x == 2)
+        //     {
+        //         cout << "two" << endl;
+        //     }
+        //     else if (x == 3)
+        //     {
+        //         cout << "three" << endl;
+        //     }
+        //     else if (x == 4)
+        //     {
+        //         cout << "four" << endl;
+        //     }
+        //     else if (x == 5)
+        //     {
+        //         cout << "five" << endl;
+        //     }
+        //     else if (x == 6)
+        //     {
+        //         cout << "six" << endl;
+        //     }
+        //     else if (x == 7)
+        //     {
+        //         cout << "seven" << endl;
+        //     }
+        //     else if (x == 8)
+        //     {
+        //         cout << "eight" << endl;
+        //     }
+        //     else if (x == 9)
+        //     {
+        //         cout << "nine" << endl;
+        //     }
+        // }
+        // else
+        // {
+        //     if (x % 2 == 0)
+        //     {
+        //         cout << "even";
+        //     }
+        //     else
+        //     {
+        //         cout << "odd";
+        //     }
+        // }
+
+        cout << i;
+    }
+
+    return 0;
+}
