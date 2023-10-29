edb97c90ac39cfb51d6b00f8b68389c1f99dd157
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-10-29 20:38:29 +0600

    loop-solved_hacker-rank

--- Module 9(Final)/For_Loop.cpp
+++ Module 9(Final)/For_Loop.cpp
@@ -1,67 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void func(int x)
+{
+    if (x == 1)
+    {
+        cout << "one" << endl;
+    }
+    else if (x == 2)
+    {
+        cout << "two" << endl;
+    }
+    else if (x == 3)
+    {
+        cout << "three" << endl;
+    }
+    else if (x == 4)
+    {
+        cout << "four" << endl;
+    }
+    else if (x == 5)
+    {
+        cout << "five" << endl;
+    }
+    else if (x == 6)
+    {
+        cout << "six" << endl;
+    }
+    else if (x == 7)
+    {
+        cout << "seven" << endl;
+    }
+    else if (x == 8)
+    {
+        cout << "eight" << endl;
+    }
+    else if (x == 9)
+    {
+        cout << "nine" << endl;
+    }
+};
+
 int main()
 {
     int n, m;
     cin >> n >> m;
     // cout << n << m;
-    for (int i = n; i < m; i++)
+    for (int i = n; i <= m; i++)
     {
         // int x = m - n;
+        if (i <= 9 && i >= 1)
+        {
+            func(i);
+        }
+        else
+        {
+            if (i % 2 == 0)
+            {
+                cout << "even" << endl;
+            }
+            else
+            {
+                cout << "odd" << endl;
+            }
+        }
 
-        // if (x <= 9 && x >= 1)
-        // {
-        //     if (x == 1)
-        //     {
-        //         cout << "one" << endl;
-        //     }
-        //     else if (x == 2)
-        //     {
-        //         cout << "two" << endl;
-        //     }
-        //     else if (x == 3)
-        //     {
-        //         cout << "three" << endl;
-        //     }
-        //     else if (x == 4)
-        //     {
-        //         cout << "four" << endl;
-        //     }
-        //     else if (x == 5)
-        //     {
-        //         cout << "five" << endl;
-        //     }
-        //     else if (x == 6)
-        //     {
-        //         cout << "six" << endl;
-        //     }
-        //     else if (x == 7)
-        //     {
-        //         cout << "seven" << endl;
-        //     }
-        //     else if (x == 8)
-        //     {
-        //         cout << "eight" << endl;
-        //     }
-        //     else if (x == 9)
-        //     {
-        //         cout << "nine" << endl;
-        //     }
-        // }
-        // else
-        // {
-        //     if (x % 2 == 0)
-        //     {
-        //         cout << "even";
-        //     }
-        //     else
-        //     {
-        //         cout << "odd";
-        //     }
-        // }
-
-        cout << i;
+        // cout << i;
     }
 
     return 0;
