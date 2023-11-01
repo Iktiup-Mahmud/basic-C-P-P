0f5c322ca80739b498a13b32f1498dc17c4553d5
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-11-01 22:31:09 +0600

    trying

--- Module 9(Final)/Variable_Sized_Arrays.cpp
+++ Module 9(Final)/Variable_Sized_Arrays.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main()
 {
-    int n, q, k;
+    int n, q;
     cin >> n >> q;
 
-    // int arr[n][n];
+    int arr[n][10000];
 
     // for (int i = 0; i < n; i++)
     // {
@@ -14,24 +14,38 @@ int main()
     //     {
     //     }
     // }
-    while (n--)
+    for (int i = 0; i < n; i++)
     {
-        for (int i = 0; i < q; i++)
+        int a;
+        cin >> a;
+        // int arr[a];
+        for (int j = 0; j < a; j++)
         {
-            int a;
-            cin >> a;
-            int arr[a];
-            for (int j = 0; j < a; j++)
-            {
-                cin >> arr[j];
-            }
-            for (int j = 0; j < a; j++)
-            {
-                cout << arr[j];
-            }
+            cin >> arr[i][j];
         }
     }
-    
+
+    // int x, y;
+    // cin >> x>> y;
+
+    // for (int i = 0; i < count; i++)
+    // {
+    //     /* code */
+    // }
+    // cout << x << y << "Abcd" << endl;
+
+    // for (int j = 0; j < a; j++)
+    // {
+    //     cout << arr[j];
+    // }
+    // cout << n << q<< endl;
+
+    while (q--)
+    {
+        int x, y;
+        cin >> x >> y;
+        cout << arr[x][y] << endl;
+    }
 
     // cout << n << q << k;
 
