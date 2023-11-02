20b40cade52786c3810ac6454e29c3c9dd1639ca
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-11-02 23:30:41 +0600

    class_and_obj-1

--- Module 9(Final)/Classes_and_Objects.cpp
+++ Module 9(Final)/Classes_and_Objects.cpp
@@ -8,7 +8,21 @@ int main()
 
     int arr[n][5];
 
-    
-      
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cout << arr[i][j];
+        }
+        cout << endl;
+    }
+
     return 0;
 }
