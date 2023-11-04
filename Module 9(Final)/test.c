64dc65bb5507843f2afa742954776a1e868dffc9
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-11-04 23:10:18 +0600

    done

--- Module 9(Final)/test.c
+++ Module 9(Final)/test.c
@@ -5,15 +5,16 @@ int main()
 
     int n;
     char a = '*';
+    
 
     scanf("%d", &n);
 
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 0; j < i; j++)
-        {
-            printf(" ");
-        }
+        // for (int j = 0; j < i; j++)
+        // {
+        //     printf(" ");
+        // }
         for (int j = 0; j < i; j++)
         {
             printf("%c", a);
