e07d1f358caeeb7442856a8f0c7159e91a668a78
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-11-04 23:22:01 +0600

    test-done

---
+++ Module 9(Final)/another-test.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 3; i++)
+    {
+        for (int j = 1; j <= 3; j++)
+        {
+            for (int k = 1; k <= 3; k++)
+            {
+                if (i == j || i == k || j == k)
+                    continue;
+                else
+                {
+                    cout << i << " " << j << " " << k << endl;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
