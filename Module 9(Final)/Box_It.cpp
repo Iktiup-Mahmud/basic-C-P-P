b8553782dcdbfbe5c8c9bc5068c37b9ed80f5b15
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-11-06 02:18:45 +0600

    box-it-3

--- Module 9(Final)/Box_It.cpp
+++ Module 9(Final)/Box_It.cpp
@@ -1,24 +1,54 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-class Box
-{
-public:
-    int l;
-    int b;
-    int h;
+// class Box
+// {
+// public:
+//     int l, b, h;
 
-    Box(int l, int b, int h)
-    {
-        this.l = l;
-        this.b = b;
-        this.h = h;
-    }
-}
+//     Box(int l, int b, int h)
+//     {
+//         this->l = l;
+//         this->b = b;
+//         this->h = h;
+//     }
 
-int
-main()
-{
+//     int getLength()
+//     {
+//         return l;
+//     }
+//     int getBreadth()
+//     {
+//         return b;
+//     }
+//     int getheight()
+//     {
+//         return h;
+//     }
+//     long long int CalculateVolume()
+//     {
+//         return l * b * h;
+//     }
+//     // bool x()
+// };
+// bool x(int a, int b)
+// {
+//     return a < b;
+// };
 
-    return 0;
-}
+// int main()
+// {
+//     Box b1(b2. , );
+//     Box b2(10, 20, 30);
+//     // cout << b1.l << b1.b << b1.h;
+//     cout << b2.getLength() << endl;
+//     cout << b2.getBreadth() << endl;
+//     cout << b2.getheight() << endl;
+//     cout << b2.CalculateVolume() << endl;
+
+//     // cout << x(b1, b2) << endl;
+
+//     cout << b2.l << " " << b2.b << " " << b2.h;
+//     return 0;
+// }
+
